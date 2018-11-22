f06d0762cb1045c3ee8e8a93a4f585529e2b41cf
Ivan Franco <ifranco@tupaca.com>
2018-11-22 15:56:05 -0300

    add readme

--- Program.cs
+++ Program.cs
@@ -23,6 +23,87 @@ namespace EjemploLinq
 
             //Join
             JoinTwoDataSources();
+
+            LinqTransformData();
+
+            PerformOperationsWithLinq();
+
+        }
+
+        private static void PerformOperationsWithLinq(){
+            double[] radii = { 1, 2, 3 };
+
+            // Query.
+            IEnumerable<string> query =
+                from rad in radii
+                select String.Format("Area = {0}", (rad * rad) * 3.14);
+
+            // Query execution. 
+            foreach (string s in query)
+                Console.WriteLine(s);
+
+            // Keep the console open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+        private static void LinqTransformData()
+        {
+            // Create the first data source.
+            List<Student> students = new List<Student>()
+        {
+            new Student { First="Svetlana",
+                Last="Omelchenko",
+                ID=111,
+                Street="123 Main Street",
+                City="Seattle",
+                Scores= new List<int> { 97, 92, 81, 60 } },
+            new Student { First="Claire",
+                Last="O’Donnell",
+                ID=112,
+                Street="124 Main Street",
+                City="Redmond",
+                Scores= new List<int> { 75, 84, 91, 39 } },
+            new Student { First="Sven",
+                Last="Mortensen",
+                ID=113,
+                Street="125 Main Street",
+                City="Lake City",
+                Scores= new List<int> { 88, 94, 65, 91 } },
+        };
+
+            // Create the second data source.
+            List<Teacher> teachers = new List<Teacher>()
+        {
+            new Teacher { First="Ann", Last="Beebe", ID=945, City="Seattle" },
+            new Teacher { First="Alex", Last="Robinson", ID=956, City="Redmond" },
+            new Teacher { First="Michiyo", Last="Sato", ID=972, City="Tacoma" }
+        };
+
+            // Create the query.
+            var peopleInSeattle = (from student in students
+                                   where student.City == "Seattle"
+                                   select student.Last)
+                        .Concat(from teacher in teachers
+                                where teacher.City == "Seattle"
+                                select teacher.Last);
+
+            Console.WriteLine("The following students and teachers live in Seattle:");
+            // Execute the query.
+            foreach (var person in peopleInSeattle)
+            {
+                Console.WriteLine(person);
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+
+
+            /* Output:
+           The following students and teachers live in Seattle:
+           Omelchenko
+           Beebe
+        */
         }
 
         private static void JoinTwoDataSources()
@@ -181,6 +262,24 @@ namespace EjemploLinq
         {
             public string Name {get;set;}
             public string City {get;set;}
+        }
+
+        private class Student
+        {
+            public string First { get; set; }
+            public string Last {get; set;}
+            public int ID { get; set; }
+            public string Street { get; set; }
+            public string City { get; set; }
+            public List<int> Scores;
+        }
+
+        private class Teacher
+        {
+            public string First { get; set; }
+            public string Last { get; set; }
+            public int ID { get; set; } 
+            public string City { get; set; }
         }
     }
 }
